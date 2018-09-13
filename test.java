116839b577e5770f484402f45520e98fb8460f96
TT <videira.theo@gmail.com>
2018-09-14 00:01:09 +0200

    Bonjour a tous les amis

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 class test{
 
 	public static void main(String[] args){
-		System.out.println("salut je teste si je dddddd peux compiler + push avec git bash");
+		System.out.println("Pourquoi libgdx quand on va utiliser environ 0 % du bordel");
 		//slip
 	}
 
