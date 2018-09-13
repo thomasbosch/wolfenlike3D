7882081d1686cd58df684d29785ae04ccd5a7787
MartinClaudel <MartinClaudel@users.noreply.github.com>
2018-09-13 20:54:07 +0200

    ouais

--- test.java
+++ test.java
@@ -1,5 +1,8 @@
-public void static main(String[] args){
+class main{
 
+public static void main(String[] args){
 	System.out.println("salut je teste si je peux compiler + push avec git bash");
+
+}
 
 }
