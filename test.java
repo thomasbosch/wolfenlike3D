e0fa745e4253d3b287cbbdc430ba79c56342a64d
TT <videira.theo@gmail.com>
2018-09-14 00:04:05 +0200

    JE TEST UN TRUC

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 class test{
 
 	public static void main(String[] args){
-		System.out.println("Pourquoi libgdx quand on va utiliser environ 0 % du bordel");
+		System.out.println("Pourquoi libgdx quand on va utiliser environ 0,000 % du bordel");
 		//slip
 	}
 
